40c3f025e5fe028223978109633e43cfdd52d5f4
Destin Moulton <destin.moulton@gmail.com>
2020-05-29 15:57:55 -0600

    Created a cli prompt for config using promptui

---
+++ internal/config/prompt.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"github.com/manifoldco/promptui"
+	"net/url"
+)
+
+func PromptForConfig() {
+	url := promptURL()
+
+	username := promptUsername()
+	fmt.Println(url)
+	fmt.Println(username)
+}
+
+func promptURL() string {
+
+	validate := func(input string) error {
+
+		if !isUrl(input) {
+			return errors.New("The URL is not valid. Should start with http:// or https://")
+		}
+		return nil
+	}
+	prompt := promptui.Prompt{
+		Label:    "Base URL",
+		Validate: validate,
+	}
+	url, err := prompt.Run()
+
+	if err != nil {
+
+	}
+
+	return url
+
+}
+
+func promptUsername() string {
+
+	prompt := promptui.Prompt{
+		Label: "Username",
+	}
+	username, err := prompt.Run()
+
+	if err != nil {
+
+	}
+
+	return username
+
+}
+
+func isUrl(str string) bool {
+	// https://stackoverflow.com/questions/31480710/validate-url-with-standard-package-in-go
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != ""
+}
