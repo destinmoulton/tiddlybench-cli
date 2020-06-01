cf169ff4b3d8b38c7ebcc8853398e663ab8a8213
Destin Moulton <destin.moulton@gmail.com>
2020-06-01 16:57:36 -0600

    Added a util file to handle tiddly to go time format

---
+++ internal/util/tiddlywikitimeformat.go
@@ -0,0 +1,63 @@
+package util
+
+import (
+	"strings"
+)
+
+type tiddlyDateFormat = string
+type goTimeFormat = string
+
+var conversionMap = map[tiddlyDateFormat]goTimeFormat{
+	"DDD":   "Monday",
+	"ddd":   "Mon",
+	"DD":    "1",
+	"0DD":   "01",
+	"DDth":  "",
+	"WW":    "",
+	"0WW":   "",
+	"YYYY":  "2006",
+	"YY":    "06",
+	"hh":    "15",
+	"0hh":   "",
+	"hh12":  "3",
+	"0hh12": "03",
+	"mm":    "4",
+	"0mm":   "04",
+	"ss":    "5",
+	"0ss":   "05",
+	"XXX":   "",
+	"0XXX":  "",
+	"am":    "pm",
+	"pm":    "pm",
+	"AM":    "PM",
+	"PM":    "PM",
+	"TZD":   "-0700",
+	"[UTC]": "",
+}
+
+// FindIncompatibleTiddlyFormats returns a slice of the
+// tiddly times not supported by this app (go)
+func FindIncompatibleTiddlyFormats(tiddlytime string) []string {
+	incompats := []string{}
+
+	for tidformat, goformat := range conversionMap {
+		if goformat == "" {
+			incompats = append(incompats, tidformat)
+		}
+	}
+	return incompats
+}
+
+// ConvertTiddlyTimeToGo replaces the tiddly time format
+// codes with the go format codes
+func ConvertTiddlyTimeToGo(tiddlytime string) string {
+
+	gotime := tiddlytime
+	for tidformat, goformat := range conversionMap {
+		if goformat != "" {
+
+			strings.ReplaceAll(gotime, tidformat, goformat)
+		}
+	}
+	return gotime
+}
