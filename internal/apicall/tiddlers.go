ea0bb1a22872ec78893a4a5f52695b4b35fe5aaf
Destin Moulton <destin.moulton@gmail.com>
2020-06-02 14:37:43 -0600

    Added UpdateTiddler method

--- internal/apicall/tiddlers.go
+++ internal/apicall/tiddlers.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 )
 
+// SingleTiddler represents a single tiddler
 type SingleTiddler struct {
 	Title    string `json:"title"`
 	Created  string `json:"created"`
@@ -17,6 +18,12 @@ type SingleTiddler struct {
 	Revision string `json:"revision"`
 }
 
+type MinimalSingleTiddler struct {
+	Title   string `json:"title"`
+	Creator string `json:"creator"`
+	Text    string `json:"text"`
+}
+
 // GetAllTiddlers returns the tiddlers from the API
 func (apicall *APICall) GetAllTiddlers() string {
 	uri := "/recipes/default/tiddlers.json"
@@ -24,8 +31,8 @@ func (apicall *APICall) GetAllTiddlers() string {
 	return apicall.Get(uri)
 }
 
+// GetTiddlerByName gets a tiddler from the api by name
 func (apicall *APICall) GetTiddlerByName(tiddler string) SingleTiddler {
-	apicall.log.Info("GetTiddlerByName", tiddler)
 	path := url.PathEscape(tiddler)
 	uri := "/recipes/default/tiddlers/" + path
 
@@ -34,4 +41,31 @@ func (apicall *APICall) GetTiddlerByName(tiddler string) SingleTiddler {
 	var tiddlerJSON SingleTiddler
 	json.Unmarshal([]byte(resp), &tiddlerJSON)
 	return tiddlerJSON
+}
+
+// AddNewTiddler puts a new tiddler to the api
+func (apicall *APICall) AddNewTiddler(title string, creator string, text string) bool {
+	putpath := url.PathEscape(title)
+
+	uri := "/recipes/default/tiddlers/" + putpath
+
+	tiddler := MinimalSingleTiddler{
+		Title:   title,
+		Creator: creator,
+		Text:    text,
+	}
+	return apicall.Put(uri, tiddler)
+}
+
+// UpdateTiddler PUTs a new version of the text to the server
+func (apicall *APICall) UpdateTiddler(title string, text string) bool {
+
+	putpath := url.PathEscape(title)
+
+	uri := "/recipes/default/tiddlers/" + putpath
+
+	tiddler := MinimalSingleTiddler{
+		Text: text,
+	}
+	return apicall.Put(uri, tiddler)
 }
