b8466d9d85619e10818a7ada43f4bc9fc753e7c2
Destin Moulton <destin.moulton@gmail.com>
2020-06-02 14:41:24 -0600

    Added TestURL method

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"fmt"
+	"net/http"
 	"net/url"
 )
 
@@ -9,4 +11,16 @@ func IsURL(str string) bool {
 	// https://stackoverflow.com/questions/31480710/validate-url-with-standard-package-in-go
 	u, err := url.Parse(str)
 	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+func TestURL(url string) bool {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error received in http resp")
+		fmt.Println(err)
+		return false
+	}
+	fmt.Println(resp)
+	return true
 }
