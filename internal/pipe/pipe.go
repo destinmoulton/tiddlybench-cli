58697e41c2359b44bb315dcc99411906c907a5fc
Destin Moulton <destin.moulton@gmail.com>
2020-06-03 15:22:27 -0600

    Created a pipe package

---
+++ internal/pipe/pipe.go
@@ -0,0 +1,61 @@
+package pipe
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"tiddly-cli/internal/logger"
+)
+
+var (
+	isPipeSet = false
+	data      []rune
+)
+
+// Pipe is an interface for the stdin pipe
+type Pipe struct {
+	log logger.Logger
+}
+
+// New creates a new Pipe
+func New(log logger.Logger) *Pipe {
+	p := new(Pipe)
+	p.log = log
+
+	p.setup()
+	return p
+}
+
+func (p *Pipe) setup() {
+	f, err := os.Stdin.Stat()
+	if err != nil {
+		p.log.Fatal("Error reading the pipe from stdin")
+	}
+
+	if f.Mode()&os.ModeNamedPipe != 0 {
+		isPipeSet = true
+		p.readData()
+	}
+}
+
+func (p *Pipe) readData() {
+	r := bufio.NewReader(os.Stdin)
+
+	for {
+		input, _, err := r.ReadRune()
+		if err != nil && err == io.EOF {
+			break
+		}
+		data = append(data, input)
+	}
+}
+
+// IsPipeSet returns bool for checking if piping
+func (p *Pipe) IsPipeSet() bool {
+	return isPipeSet
+}
+
+// Get returns the string of stdin data
+func (p *Pipe) Get() string {
+	return string(data)
+}
