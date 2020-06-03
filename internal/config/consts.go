5164345875fbab55722dc7f6610e5c9efab4bf74
Destin Moulton <destin.moulton@gmail.com>
2020-06-02 20:51:14 -0600

    Created a consts file

---
+++ internal/config/consts.go
@@ -0,0 +1,18 @@
+package config
+
+const (
+	// URL config setting
+	URL = "URL"
+	// TitleTemplate config setting
+	TitleTemplate = "TitleTemplate"
+	// Username config setting
+	Username = "Username"
+	// SavePassword config setting
+	SavePassword = "SavePassword"
+	// Password config setting
+	Password = "Password"
+	// Yes definition
+	Yes = "yes"
+	// No Definition
+	No = "no"
+)
