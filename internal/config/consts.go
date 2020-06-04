df5f6b5cd9fc18899cc65aca003ae5f491cc8cc9
Destin Moulton <destin.moulton@gmail.com>
2020-06-04 09:56:45 -0600

    Added consts

--- internal/config/consts.go
+++ internal/config/consts.go
@@ -1,18 +1,34 @@
 package config
 
+// Define the config keys
 const (
-	// URL config setting
-	URL = "URL"
-	// TitleTemplate config setting
-	TitleTemplate = "TitleTemplate"
-	// Username config setting
-	Username = "Username"
-	// SavePassword config setting
-	SavePassword = "SavePassword"
-	// Password config setting
-	Password = "Password"
-	// Yes definition
-	Yes = "yes"
-	// No Definition
-	No = "no"
+
+	// CKURL config setting
+	CKURL = "url"
+	// CKDestinations config key
+	CKDestinations = "destinations"
+	// CKInbox is the title of the inbox config entry
+	CKInbox = "inbox"
+	// CKJournal is the title of the journal config entry
+	CKJournal = "journal"
+	// CKTags config key
+	CKTags = "tags"
+	// CKTitleTemplate config key
+	CKTitleTemplate = "title_template"
+	// CKUsername config setting
+	CKUsername = "username"
+	// CKShouldSavePassword config setting
+	CKShouldSavePassword = "should_save_password"
+	// CKPassword config setting
+	CKPassword = "password"
+	// CKYes definition
+	CKYes = "yes"
+	// CKNo Definition
+	CKNo = "no"
+	// CKBlocks config key
+	CKBlocks = "blocks"
+	// CKBegin config key
+	CKBegin = "begin"
+	// CKEnd config key
+	CKEnd = "end"
 )
